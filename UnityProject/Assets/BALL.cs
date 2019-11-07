296e9c83f4955f7a310ae7381e29617abe31baca
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-11-07 11:24:00 +0800

    完成測試

---
+++ UnityProject/Assets/BALL.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class BALL : MonoBehaviour
+{
+    [Header("Move Speed")]
+    public float speed = 20f;
+    [Header("Jump Height")]
+    public float jump = 50f;
+    public bool isGround = false;
+
+
+    private Transform trf;
+    private Rigidbody r3d;
+   
+
+
+    private void Start()
+    {
+        trf = GetComponent<Transform>();
+        r3d = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("碰到" + collision.gameObject);
+    }
+
+                                     /// <summary>
+                                    /// 走路
+                                    /// </summary>
+    void Walk()
+    {
+        r3d.AddForce(new Vector3(-speed * (Input.GetAxis("Vertical")), 0,speed  * (Input.GetAxis("Horizontal"))));
+    }
+                                  /// <summary>
+                                  /// 跳躍
+                                  /// </summary>
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = true;
+            r3d.AddForce(new Vector3(0, jump,0 ));
+        }
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    /// <param></param>
+    private void Jump(int jump = 0)
+    {
+        transform.eulerAngles = new Vector3(0, jump, 0);
+    }
+}
---
+++ UnityProject/Assets/RO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RO : MonoBehaviour
+{
+    [Header("任務對話")]
+    public string star = "嗨,能幫助我一個忙嗎?";
+    [Header("對話速率")]
+    public float talkspeed = 1.5f;
+    [Header("任務")]
+    public bool mission_complete = false;
+    public int batery = 0;
+    public int bateryfinish = 5;
+
+
+    void Start()
+    {
+        Debug.Log(star);
+    }
+
+
+    void Update()
+    {
+
+    }
+}
